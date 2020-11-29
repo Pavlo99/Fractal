6c05bb6eb95d3de891a8acbea600671541d23300
Павло <plazyu13@gmail.com>
2020-11-29 17:55:06 +0200

    Add project files.

---
+++ Fractal/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.File = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1101, 598);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.draw);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(594, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Design";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1101, 55);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(372, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ітерації -";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(460, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.File);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1101, 30);
+            this.panel3.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(141, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Help";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Setting";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // File
+            // 
+            this.File.Location = new System.Drawing.Point(0, 0);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(75, 30);
+            this.File.TabIndex = 0;
+            this.File.Text = "File";
+            this.File.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 682);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button File;
+    }
+}
+
---
+++ Fractal/Form1.cs
@@ -0,0 +1,85 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        Pen myPen = new Pen(Color.Red);
+        Graphics graphics = null;
+
+        static int start_x, start_y;
+        static int iteration = 9;
+
+        private void draw(object sender, PaintEventArgs e)
+        {
+            myPen.Width = 1;
+            graphics = panel1.CreateGraphics();
+            DrawDragonLine(graphics, iteration, Direction.Right, start_x, start_y, 200, 200);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            start_x = panel1.Width / 2;
+            start_y = panel1.Height / 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start_x = panel1.Width / 2;
+            start_y = panel1.Height / 3;
+
+            if(textBox1.Text != "") iteration = Int32.Parse(textBox1.Text);
+
+            panel1.Refresh();
+        }
+        private void DrawDragonLine(Graphics gr, int level, Direction turn_towards, float x1, float y1, float dx, float dy)
+        {
+            if (level <= 0)
+            {
+                graphics.DrawLine(Pens.Red, x1, y1, x1 + dx, y1 + dy);
+            }
+            else
+            {
+                float nx = (float)(dx / 2);
+                float ny = (float)(dy / 2);
+                float dx2 = -ny + nx;
+                float dy2 = nx + ny;
+                if (turn_towards == Direction.Right)
+                {
+                    // Turn to the right.
+                    DrawDragonLine(gr, level - 1, Direction.Right,
+                        x1, y1, dx2, dy2);
+                    float x2 = x1 + dx2;
+                    float y2 = y1 + dy2;
+                    DrawDragonLine(gr, level - 1, Direction.Left,
+                        x2, y2, dy2, -dx2);
+                }
+                else
+                {
+                    // Turn to the left.
+                    DrawDragonLine(gr, level - 1, Direction.Right,
+                        x1, y1, dy2, -dx2);
+                    float x2 = x1 + dy2;
+                    float y2 = y1 - dx2;
+                    DrawDragonLine(gr, level - 1, Direction.Left,
+                        x2, y2, dx2, dy2);
+                }
+            }
+        }
+    }
+
+    enum Direction { 
+        Right,
+        Left
+    }
+}
